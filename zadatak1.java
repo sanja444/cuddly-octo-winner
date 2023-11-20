e61134203f6847d5c61c0d071452aadd2da79098
Sanja <cburn995@gmail.com>
2023-11-20 15:30:04 +0100

    Inital Commit

--- zadatak1.java
+++ zadatak1.java
@@ -2,8 +2,10 @@ public class zadatak1 {
 	public static void main(String [] args){
 		for(int i=0;i<36;i++){
 		System.out.print("Hello World");
+	
 		
 		}
+		System.out.print("Hello again");
 	
 	}
 
